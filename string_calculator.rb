9c1506e35528f1832c9994a4d837aaa383d671ca
Anul <anul@alphabyte.com>
2025-02-13 21:05:15 +0530

    test cases

--- string_calculator.rb
+++ string_calculator.rb
@@ -14,7 +14,14 @@ def add(numbers)
 		total = 0
 	else
 		total = numbers.split(delimiter).map {|n| n.split("\n")}.flatten
-		total = total.map {|n| n.to_i}.inject(&:+)
+		total = total.map {|n|
+			n = n.to_i
+			if n < 0
+				puts "negative numbers are not allowed"
+				return
+			end
+			n
+		}.inject(&:+)
 	end
 	total
 end
@@ -24,3 +31,33 @@ puts 'input: "1", output: '+add("1").to_s
 puts 'input: "1,5", output: '+add("1,5").to_s
 puts 'input: "1\n2,3", output: '+add("1\n2,3").to_s
 puts 'input: "//;\n1;2", output: '+add("//;\n1;2").to_s
+puts add("1,-5")
+
+
+### TDD
+
+require 'minitest/autorun'
+
+describe "string calculator"  do
+  it "returns 0 when input is empty string"  do
+	  assert_equal(add(""), 0)
+  end
+  it "returns 1 when input is '1'"  do
+	  assert_equal(add("1"), 1)
+  end
+  it "returns 6 when input is '1,5'"  do
+	  assert_equal(add("1,5"), 6)
+  end
+  it "returns 6 when input is '1\n2,3'"  do
+	  assert_equal(add("1\n2,3"), 6)
+  end
+  it "returns 3 when input is '//;\n1;2'"  do
+	  assert_equal(add("//;\n1;2"), 3)
+  end
+  it "returns 3 when input is negative numbers"  do
+	  assert_equal(add("//;\n1;2"), 3)
+  end
+  it "returns nil when input contains negative number"  do
+	  assert_equal(add("1,-5"), nil)
+  end
+end
